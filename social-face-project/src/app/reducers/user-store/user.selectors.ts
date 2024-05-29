8e7081d736af5565700354c011a31779d394e751
diegoGarciaUnosquare <85252487+diegoGarciaUnosquare@users.noreply.github.com>
2024-05-29 14:17:34 -0600

    Delete social-face-project directory

--- social-face-project/src/app/reducers/user-store/user.selectors.ts
+++
@@ -1,9 +0,0 @@
-import { AppState } from "./user.reducer";
-import { createSelector } from "@ngrx/store";
-
-export const state = (state: AppState) => state;
-
-export const getErrors = createSelector(
-    state,
-    (state: AppState) => state.error ? state.error : null,
-);
--- social-face-project/unit-tests/mocks/services/snackbar-service-mocks.ts
+++
@@ -1,5 +0,0 @@
-class SnackbarServiceMock {
-  openSnackBar: jasmine.Spy = jasmine.createSpy('openSnackBar');
-}
-
-export default SnackbarServiceMock;
