ed6c549805d1391fc02c7233251acc1be4fb2b1f
diegoGarciaUnosquare <diegoGarciaUnosquare@users.noreply.github.com>
2024-06-12 02:57:09 +0000

    deploy: 4b5056ec94792f8096628807be37051fbb027bec

---
+++ chunk-T6GQX6WA.js
@@ -0,0 +1,1 @@
+import{b as t,c as e}from"./chunk-UO3TCGKE.js";var g=t("[Settings page] Get Settings",e()),n=t("[Settings page] Get Settings Success",e()),i=t("[Settings page] Get Settings Failure",e()),S=t("[Settings page] Update Settings",e()),p=t("[Settings page] Update Settings Success",e()),a=t("[Settings page] Update Settings Failure",e());export{g as a,n as b,i as c,S as d,p as e,a as f};
